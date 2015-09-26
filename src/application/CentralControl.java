bc66e17986b97a960ca466257f9af796ca3145e3
chrolss <christian.olsson01@gmail.com>
2015-09-26 19:01:56 +0200

    First push

---
+++ src/application/CentralControl.java
@@ -0,0 +1,44 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
+public class CentralControl extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+
+			GridPane grid = new GridPane();
+			Scene scene = new Scene(grid,800,600);
+			grid.setId("pane");
+			
+			//Text which displays the connection status of BBB
+			Text connectionStatus = new Text();
+			grid.add(connectionStatus, 6, 0);
+			connectionStatus.setText("Disconnected");
+			connectionStatus.setFill(Color.RED);
+			
+			//Buttons
+			Button connectButton = new Button();
+			connectButton.setText("Connect");
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+	}
+
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/Diplomat.java
@@ -0,0 +1,11 @@
+package application;
+
+
+//Communication class which transmits and receives information from the BBB
+public class Diplomat {
+
+	public Diplomat() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
